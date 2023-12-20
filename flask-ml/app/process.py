752371940a33c6e4bb41eb3dcb41d290860089be
yosalx <yoseph141002@gmail.com>
2023-12-20 17:04:36 +0700

    update flask and model

--- flask-ml/app/process.py
+++ flask-ml/app/process.py
@@ -4,24 +4,35 @@ import numpy as np
 import urllib
 from tensorflow.keras.preprocessing import image
 from io import BytesIO
+from flask import jsonify
 
 def predictImage(url):
     try:
-        model = tf.keras.models.load_model("./app/model.h5")
+        model = tf.keras.models.load_model("gs://bloody-happy-bucket/model/model.h5")
+
+        print("Predicting from image url:", url)
 
         with urllib.request.urlopen(url) as data:
             img1 = image.load_img(BytesIO(data.read()), target_size=(150, 150))
     
-        Y = image.img_to_array(img1)        
+        Y = image.img_to_array(img1)
         X = np.expand_dims(Y,axis=0)
         X_normalized = X / 255.0
 
         value_predicted = model.predict(X_normalized)
 
         if value_predicted >= 0.5:
-            return 'Non Anemia'
+            print("Prediction returned Non Anemia")
+            data = {
+                'prediction' : 'Non Anemia'
+            }
+            return jsonify(data)
         else:
-            return 'Anemia'
+            print("Prediction returned Anemia")
+            data = {
+                'prediction' : 'Anemia'
+            }
+            return jsonify(data)
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
--- flask-ml/app/routes.py
+++ flask-ml/app/routes.py
@@ -11,6 +11,7 @@ def home():
 def prediction():
     if request.method == 'POST':
         request_data = request.get_json()
+        print("Predicting request:", request)
         file_link = request_data["file_link"]
         x=predictImage(file_link)
         return x
